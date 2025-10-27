0a95f4db83b3d53ba45d01745f4970e4ba476b90
Alithebeast9999 <alimuslimov7@gmail.com>
2025-10-27 20:32:35 +0300

    Update db.py

--- db.py
+++ db.py
@@ -2,20 +2,30 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect("data.sqlite")
+    conn = sqlite3.connect("finance.db")
     c = conn.cursor()
-    c.execute("""CREATE TABLE IF NOT EXISTS expenses (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        category TEXT,
-        amount REAL,
-        date TEXT
-    )""")
-    c.execute("""CREATE TABLE IF NOT EXISTS category_limits (
-        user_id INTEGER,
-        category TEXT,
-        limit REAL,
-        PRIMARY KEY (user_id, category)
-    )""")
+
+    # Таблица с лимитами по категориям
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS category_limits (
+            user_id INTEGER,
+            category TEXT,
+            limit_amount REAL,
+            PRIMARY KEY (user_id, category)
+        )
+    """)
+
+    # Таблица с расходами
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            category TEXT,
+            amount REAL,
+            date TEXT
+        )
+    """)
+
     conn.commit()
     conn.close()
+
