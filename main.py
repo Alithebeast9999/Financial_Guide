5a2413880d90b44afdf87d50684476fd94adbb2b
Alithebeast9999 <alimuslimov7@gmail.com>
2025-11-07 10:49:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ from apscheduler.triggers.cron import CronTrigger
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import Update as TgUpdate
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
